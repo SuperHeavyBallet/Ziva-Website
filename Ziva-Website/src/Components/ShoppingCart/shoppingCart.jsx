6dde56139fc9bcb73bb2dd0a67ac7fb3c3fed9c4
Alexander Gorham <Superheavyballet@gmail.com>
2024-07-18 01:09:00 +0200

    Fix Catalogue page not loading without productID present

--- Ziva-Website/src/Components/ShoppingCart/shoppingCart.jsx
+++ Ziva-Website/src/Components/ShoppingCart/shoppingCart.jsx
@@ -42,6 +42,7 @@ export default function ShoppingCart( {cartContents, onRemoveItem, itemClicked, 
     useEffect(() =>
     {
         calculateTotalPrice();
+        
     }, [cartContents] );
 
     return(
